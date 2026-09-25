4dc5eab10a9a9cbdfa74df4623267c49c101d064
IsraelCIL <israel9390@gmail.com>
2026-09-25 15:37:01 +0300

    release: bump bridge version to 0.1.0-alpha.9

--- driver/src/core/version.lua
+++ driver/src/core/version.lua
@@ -1,6 +1,6 @@
 local Version = {}
 
-Version.BRIDGE_VERSION = "0.1.0-alpha.8"
+Version.BRIDGE_VERSION = "0.1.0-alpha.9"
 Version.PROTOCOL_VERSION = 1
 Version.MIN_OS = { major = 3, minor = 3, patch = 0 }
 
